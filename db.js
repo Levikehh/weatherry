5e9b231f35dab3ff3c12febe784ada51a4329f09
Dóczi Levente <bkalevi@gmail.com>
2022-11-17 16:01:06 +0100

    Save response to redis

---
+++ db.js
@@ -0,0 +1,14 @@
+const redis = require("redis");
+
+let client;
+
+const getClient = async () => {
+  if (client) return client;
+
+  client = redis.createClient();
+  client.on("error", (error) => console.error(`Redis Error : ${error}`));
+  await client.connect();
+  return client
+};
+
+module.exports = getClient;
--- routes/weather.js
+++ routes/weather.js
@@ -1,21 +1,48 @@
 const router = require("express").Router();
 const axios = require("axios");
+const db = require("../db");
 
 const CITY_DATA = require("../constants");
 
 const fetchDataFromAPI = async (city) => {
-  const {data} = await axios({
+  console.log("Fetching api");
+  const { data } = await axios({
     url: `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&appid=${process.env.OPENWEATHERMAP_API_KEY}`,
     method: "GET",
-  })
+  });
 
-  console.log(data)
-  return data
-}
+  return data;
+};
 
-const getDataFromDB = async () => {
-  
-}
+const getDataFromDB = async (city) => {
+  const client = await db();
+  const storedData = await client.get(city.local_names.hu.toLowerCase());
+
+  if (!storedData) {
+    console.log("No cached data");
+    response = await fetchDataFromAPI(city);
+    await client.set(
+      city.local_names.hu.toLowerCase(),
+      JSON.stringify(response)
+    );
+  }
+
+  let response = JSON.parse(storedData);
+  const now = Date.now()
+  const lastUpdate = response.dt * 1000
+  const timeDifference = 10 * 60 * 1000 // 10 minutes
+
+  if ((now - lastUpdate) >= timeDifference){
+    console.log("Data is outdated");
+    response = await fetchDataFromAPI(city);
+    await client.set(
+      city.local_names.hu.toLowerCase(),
+      JSON.stringify(response)
+    );
+  }
+
+  return response;
+};
 
 router.get("/:city", async (req, res) => {
   console.log("Called weather route");
@@ -37,7 +64,7 @@ router.get("/:city", async (req, res) => {
       data: `Please select another city as we don't have data for '${req.params.city}'`,
     });
 
-  const response = await fetchDataFromAPI(selectedCity)
+  const response = await getDataFromDB(selectedCity);
   res.status(200).json({ status: 200, error: false, data: response });
 });
 
